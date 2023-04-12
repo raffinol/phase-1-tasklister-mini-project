93b155a55009761200fe5eb7b5bddfc2f77e1241
Rafael Finol <rafaelfinol88@gmail.com>
2023-04-12 15:31:49 -0400

    updated lab

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener('submit', (e) =>{
   e.preventDefault();
   let p = document.createElement('p');
-  p.textContent = ("- " + e.target['new-task-description'].value + " ");
+  let description = e.target['new-task-description'].value
+  const select = document.getElementById("select")
+  if ( description === ""){
+   p.textContent = ("- " + select.value + " ");
+    } else {
+  p.textContent = ("- " + description + " ");
+  }
   document.querySelector('#tasks').appendChild(p)
   form.reset()
   let btn = document.createElement('button')
