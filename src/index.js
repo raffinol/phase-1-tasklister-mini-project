fd34d7c8b3b41a393e4b35f6a12ea3b639782b89
Rafael Finol <rafaelfinol88@gmail.com>
2023-04-12 11:55:09 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form') 
+  form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  let p = document.createElement('p');
+  p.textContent = ("- " + e.target['new-task-description'].value + " ");
+  document.querySelector('#tasks').appendChild(p)
+  form.reset()
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask);
+  btn.textContent = 'X'
+  p.appendChild(btn)
+  });
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+  }
+
 });
